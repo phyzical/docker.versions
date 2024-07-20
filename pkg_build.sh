ab866d58d7c1afa157bada420660149de64e40a1
phyzical <5182053+phyzical@users.noreply.github.com>
2024-07-21 03:23:47 +0800

    inital push

--- pkg_build.sh
+++ pkg_build.sh
@@ -4,24 +4,22 @@ CWD=$(pwd)
 tmpdir="$CWD/tmp/tmp.$(($RANDOM * 19318203981230 + 40))"
 version=$(date +"%Y.%m.%d")
 filename="$CWD/archive/docker.versions-$version.txz"
+rm "$filename"
 dayversion=$(ls "$CWD"/archive/docker.versions-"$version"*.txz 2>/dev/null | wc -l)
 
 if [ "$dayversion" -gt 0 ]; then
     filename=$CWD/archive/docker.versions-$version.$dayversion.txz
 fi
-
 mkdir -p "$tmpdir"
-chmod 0755 -R .
 
-cd "$CWD/src/docker.versions" || exit
-cp --parents -f $(find . -type f ! \( -iname "pkg_build.sh" -o -iname "sftp-config.json" \)) "$tmpdir"/
+rsync -av --progress . "$tmpdir" --exclude .git --exclude tmp --exclude .env --exclude archive
 
 cd "$tmpdir" || exit
-makepkg -l y -c y "$filename"
 
-cd "$CWD" || exit
-rm -R "$CWD"/tmp
-chmod 0755 -R .
+tar -cJf "$filename" .
 
-echo "MD5:"
-md5sum "$filename"
+cd - || exit
+
+rm -rf "$tmpdir"
+
+echo "MD5: $(md5sum "$filename")"
