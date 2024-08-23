0b0d70adfe81067768b611e6d393bd4ca1b4b072
phyzical <5182053+phyzical@users.noreply.github.com>
2024-08-23 19:26:17 +0800

    bump version

--- pkg_build.sh
+++ pkg_build.sh
@@ -25,6 +25,3 @@ rm -rf "$tmpdir"
 sed -i '' 's/<!ENTITY version ".*">/<!ENTITY version "'"$version"'">/' docker.versions.plg
 md5hash=$(md5 -q "$filename")
 sed -i '' 's/<!ENTITY md5 ".*">/<!ENTITY md5 "'"$md5hash"'">/' docker.versions.plg
-
-echo "MD5: $(md5sum "$filename")"
-echo "once pushed install via https://raw.githubusercontent.com/phyzical/docker.versions/main/docker.versions.plg"
