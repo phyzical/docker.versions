f79341ac6c81f078fd608c0968c23b4905ee2737
phyzical <5182053+phyzical@users.noreply.github.com>
2024-10-01 13:25:21 +0800

    avoid adding empty duplicated changelogs

--- src/docker.versions/usr/local/emhttp/plugins/docker.versions/server/services/Containers.php
+++ src/docker.versions/usr/local/emhttp/plugins/docker.versions/server/services/Containers.php
@@ -143,22 +143,25 @@ class Containers
                             "<summary><a target=\"blank\" href=\"{$primaryRelease->htmlUrl}\">{$primaryRelease->tagName} ($primaryRelease->createdAt)</a></summary>",
                         ];
                         if (!empty($primaryRelease->extraReleases)) {
-                            $detailsChunks = array_merge(
-                                $detailsChunks,
-                                [
-                                    "<details>",
-                                    "<summary>Duplicate changelogs</summary>"
-                                ],
-                                array_map(
-                                    function ($extraRelease) {
-                                        return "<a target=\"blank\" href=\"{$extraRelease->htmlUrl}\">{$extraRelease->tagName} ({$extraRelease->createdAt})</a>";
-                                    },
-                                    array_filter($primaryRelease->extraReleases, function ($extraRelease) use ($currentImageCreatedAt, $currentImageSourceTag) {
-                                        return strtotime($extraRelease->createdAt) > strtotime($currentImageCreatedAt) && $extraRelease->tagName != $currentImageSourceTag;
-                                    })
-                                ),
-                                ["</details>"]
-                            );
+                            $filteredDuplicates = array_filter($primaryRelease->extraReleases, function ($extraRelease) use ($currentImageCreatedAt, $currentImageSourceTag) {
+                                return strtotime($extraRelease->createdAt) > strtotime($currentImageCreatedAt) && $extraRelease->tagName != $currentImageSourceTag;
+                            });
+                            if (!empty($filteredDuplicates)) {
+                                $detailsChunks = array_merge(
+                                    $detailsChunks,
+                                    [
+                                        "<details>",
+                                        "<summary>Duplicate changelogs</summary>"
+                                    ],
+                                    array_map(
+                                        function ($extraRelease) {
+                                            return "<a target=\"blank\" href=\"{$extraRelease->htmlUrl}\">{$extraRelease->tagName} ({$extraRelease->createdAt})</a>";
+                                        },
+                                        $filteredDuplicates
+                                    ),
+                                    ["</details>"]
+                                );
+                            }
                         }
 
                         $detailsChunks = array_merge(
