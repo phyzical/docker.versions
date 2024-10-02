191891ed09fe6759d1261abc38ed81ff207654b0
phyzical <5182053+phyzical@users.noreply.github.com>
2024-10-02 14:58:16 +0800

    add workflow

--- pkg_build.sh
+++ pkg_build.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
+plugin_name="docker.versions"
 CWD=$(pwd)
 tmpdir="$CWD/tmp/tmp.$(($RANDOM * 19318203981230 + 40))"
 version=$(date +"%Y.%m.%d")
-filename="$CWD/archive/docker.versions-$version.txz"
+filename="$CWD/archive/$plugin_name-$version.txz"
 rm "$filename"
-dayversion=$(ls "$CWD"/archive/docker.versions-"$version"*.txz 2>/dev/null | wc -l)
+dayversion=$(ls "$CWD"/archive/$plugin_name-"$version"*.txz 2>/dev/null | wc -l)
 
 if [ "$dayversion" -gt 0 ]; then
-    filename=$CWD/archive/docker.versions-$version.$dayversion.txz
+    filename=$CWD/archive/$plugin_name-$version.$dayversion.txz
 fi
 mkdir -p "$tmpdir"
 
-rsync -av --progress ./src/docker.versions/ "$tmpdir" --exclude .git --exclude tmp --exclude .env --exclude archive
+rsync -av --progress src/$plugin_name/ "$tmpdir" --exclude .git --exclude tmp --exclude .env --exclude archive
 
 cd "$tmpdir" || exit
 
-tar -cJf "$filename" .
+tar_command="tar"
+if [[ "$(uname)" == "Darwin" ]]; then
+    tar_command="gtar"
+fi
+$tar_command --owner=0 --group=0 -cJf "$filename" .
 
 cd - || exit
 
 rm -rf "$tmpdir"
 
-sed -i '' 's/<!ENTITY version ".*">/<!ENTITY version "'"$version"'">/' docker.versions.plg
+sed -i '' 's/<!ENTITY version ".*">/<!ENTITY version "'"$version"'">/' $plugin_name.plg
 md5hash=$(md5 -q "$filename")
-sed -i '' 's/<!ENTITY md5 ".*">/<!ENTITY md5 "'"$md5hash"'">/' docker.versions.plg
+sed -i '' 's/<!ENTITY md5 ".*">/<!ENTITY md5 "'"$md5hash"'">/' $plugin_name.plg
+
+echo "MD5: $(md5sum "$filename")"
+echo "once pushed install via https://raw.githubusercontent.com/phyzical/$plugin_name/main/$plugin_name.plg"
+
+$tar_command -tvf "$filename"
